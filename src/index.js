8855eb074920ecffb91ce9f7e97b30b4398b466c
Max Kovalenko <tutmrmaxi@gmail.com>
2021-08-16 20:18:54 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!Array.isArray(array) || !array.length) { return 0 };
+
+  return array.sort((a, b) => a - b)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!Array.isArray(array) || !array.length) { return 0 };
+
+  return array.sort((a, b) => a - b)[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || !array.length) { return 0 };
+
+  let sum = 0;
+  for (i = 0; i <= array.length - 1; i++) {
+    sum += array[i];
+  }
+  return (sum / array.length);
 }
